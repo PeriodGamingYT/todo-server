d7be5da8dc1cd3d31cc75bf3d9ce64f355383e80
PeriodGamingYT <54007341+PeriodGamingYT@users.noreply.github.com>
2024-08-08 16:39:48 -0600

    json can now be loaded into server data from "data.json"

--- server/server.go
+++ server/server.go
@@ -2,10 +2,12 @@ package server
 
 import (
 	"os"
-	"fmt"
 	"io"
-	"net/http"
+	"fmt"
 	"net"
+	"errors"
+	"net/http"
+	"encoding/json"
 )
 
 type ChecklistItem struct {
@@ -27,7 +29,72 @@ type ServerData struct {
 	inventory map[string]InventoryItem
 }
 
-func (serverData ServerData) LoadPassword() error {
+type checklistItemJson struct {
+	Checked bool `json:"checked"`
+	Index int `json:"index"`
+}
+
+type inventoryItemJson struct {
+	Current int `json:"current"`
+	Max int `json:"max"`
+	Index int `json:"index"`
+}
+
+type serverDataJson struct {
+	Checklist map[string]checklistItemJson `json:"checklist"`
+	Inventory map[string]inventoryItemJson `json:"inventory"`
+}
+
+func (serverData *ServerData) Clear() {
+	for key := range serverData.checklist { delete(serverData.checklist, key) }
+	for key := range serverData.inventory { delete(serverData.inventory, key) }
+	serverData.checklist = make(map[string]ChecklistItem)
+	serverData.inventory = make(map[string]InventoryItem)
+}
+
+func (serverData *ServerData) LoadJSONBytes(bytes []byte) error {
+	var serverJson serverDataJson
+	err := json.Unmarshal(bytes, &serverJson)
+	if err != nil { return err }
+	serverData.Clear()
+	for key := range serverJson.Checklist {
+		item := serverJson.Checklist[key]
+		serverData.checklist[key] = ChecklistItem {
+			key,
+			item.Checked,
+			item.Index,
+		}
+	}
+
+	for key := range serverJson.Inventory {
+		item := serverJson.Inventory[key]
+		serverData.inventory[key] = InventoryItem {
+			key,
+			item.Current,
+			item.Max,
+			item.Index,
+		}
+	}
+
+	return nil
+}
+
+func (serverData *ServerData) LoadJSON(filename string) error {
+	if _, err := os.Stat(filename); errors.Is(err, os.ErrNotExist) {
+		return nil
+	}
+
+	file, err := os.Open(filename)
+	if err != nil { return err }
+	defer file.Close()
+	bytes, err := io.ReadAll(file)
+	if err != nil { return err }
+	err = serverData.LoadJSONBytes(bytes)
+	if err != nil { return err }
+	return nil
+}
+
+func (serverData *ServerData) LoadPassword() error {
 	file, err := os.Open("password.txt")
 	if err != nil { fmt.Println("Can't find password.txt!"); return err }
 	defer file.Close()
@@ -37,14 +104,33 @@ func (serverData ServerData) LoadPassword() error {
 	return nil
 }
 
-func DataHandler(writer http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(writer, "hi!\n")
+type dataRequestType int
+const(
+	ClientSave dataRequestType = iota
+	ClientLoad
+)
+
+type dataRequest struct {
+	Type dataRequestType `json:"type"`
+	Checklist []ChecklistItem `json:"checklist"`
+	Inventory []InventoryItem `json:"inventory"`
+}
+
+func DataHandler(writer http.ResponseWriter, req *http.Request) error {
+	var data dataRequest
+	err := json.NewDecoder(req.Body).Decode(&data)
+	if err := nil { return err }
+
+	// TODO(ElkElan) handle that request
+	return nil
 }
 
 func Init() error {
 	fmt.Println("Server starting...")
-	var data ServerData
-	err := data.LoadPassword()
+	var serverData ServerData
+	err := serverData.LoadPassword()
+	if err != nil { return err }
+	err = serverData.LoadJSON("data.json")
 	if err != nil { return err }
 	var server http.Server
 	server.Handler = http.HandlerFunc(DataHandler)
